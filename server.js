6cacb31d208c90502fcba842c3d413223bfb7ddf
kmani314 <krishnamani314@gmail.com>
2019-06-10 13:26:10 -0700

    Write basic web server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-var app = require('express');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+  console.log("Listening on port: " + port);
+});
