0c5a6d578508a7e78a341b460ee2a477008472e9
kmani314 <krishnamani314@gmail.com>
2019-06-11 23:04:54 -0700

    Basic functionality done, now lists cameras and dynamically adds links

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const User = require('../modules/user.js');
 const path = require('path');
-const sanitize = require('mongo-sanitize')
+const sanitize = require('mongo-sanitize');
+const fs = require('fs');
+const ejs = require('ejs');
 /* This is the registration route, no longer valid after users have been created
 router.post('/', (req, res, next) => {
   if(req.body.username && req.body.password) {
@@ -37,7 +39,14 @@ router.get('/view.html', (req, res, next) => {
     } else {
       if(user === null) {
         res.redirect('/');
-      } else return res.sendFile(path.join(__dirname, '../public', 'view.html'));
+      } else {
+        var links = [];
+        var cameras = fs.readdirSync(path.join(__dirname, '../public/video/'));
+        for(var i = 0; i < cameras.length; i++) {
+          links[i] = path.join('/video/', cameras[i], 'index.m3u8');
+        }
+        return res.render('view', {cameras: cameras, links: links});
+      }
     }
   })
 });
@@ -51,4 +60,17 @@ router.get('/logout', (req, res, next) => {
     res.redirect('/');
   }
 });
+
+router.get('/:video/*', (req, res, next) => {
+  User.findById(req.session.userId).exec(function (err, user) {
+    if(err) {
+      return next(err);
+    } else {
+      if(user === null) {
+        res.redirect('/');
+      } else next();
+    }
+  })
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const session = require('express-session');
 const crypto = require('crypto');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1/webcc', { useNewUrlParser: true });
@@ -15,6 +17,9 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log("DB Opened");
 });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/public'));
 
 app.use(session({
   secret: crypto.randomBytes(64).toString('hex'),
